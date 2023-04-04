4dd5b92084df0a3abdc13d74b8fd9897bac82b49
silverturtle1 <joshjmerritt1@gmail.com>
2023-04-03 21:20:30 -0700

    Supports date range selection

--- finra.py
+++ finra.py
@@ -39,38 +39,54 @@ def get_ssdata(startdate, enddate=0):
     values = range(0)
     if enddate != 0:
         numDays = (datetime.strptime(enddate, '%Y%m%d') - datetime.strptime(startdate, '%Y%m%d')).days
+        print("numDays")
         print(numDays)
         values = range(numDays + 1)
     else:
         numDays = 0
 
     etfOnly = False
-    while True:
-        try:
-            for i in values:
-                cur_day = datetime.strptime(startdate, '%Y%m%d') + timedelta(days=i)
-                file_date = cur_day.strftime('%Y%m%d')
-                print(file_date)
-                finra_file = finra_dir + f'{file_date}.txt'
-                print(finra_file)
-                ssdata_temp = get_csv(finra_file)
-                print("FINRA File")
-                finra_df = ssdata_temp[(ssdata_temp.TotalVolume > min_volume)]
-                # if i == 0:
-                #     finra_df = temp_df
-                # else:
-                #     finra_df.append(temp_df)
 
-            print(file_date)
-            finra_file = finra_dir + f'{file_date}.txt'
-            print(finra_file)
+    print("Top of try")
+    for i in values:
+        cur_day = datetime.strptime(startdate, '%Y%m%d') + timedelta(days=i)
+        file_date = cur_day.strftime('%Y%m%d')
+        print(file_date)
+        finra_file = finra_dir + f'{file_date}.txt'
+        print(finra_file)
+        try:
             ssdata_temp = get_csv(finra_file)
             print("FINRA File")
-            finra_df = ssdata_temp[(ssdata_temp.TotalVolume > min_volume)]
+            print(i)
+            temp_df = ssdata_temp[(ssdata_temp.TotalVolume > min_volume)]
+            print(temp_df)
+            if finra_df.empty:
+                finra_df = temp_df.copy()
+                print("finra_df was empty")
+            else:
+                finra_df = finra_df.append(temp_df.copy())
 
-            # finra_df['Percentile'] = finra_df.TotalVolume.rank(pct = True)
-            # finra_df = finra_df[(finra_df.Percentile > 0.9)]
-            # print(finra_df)
+        except requests.HTTPError as e:
+            print(f"[!] Exception caught: {e}{file_date}")
+
+    print("Post Loop")
+    print(finra_df)
+    finra_df = finra_df.groupby('Symbol').sum().reset_index()
+    print(finra_df)
+
+    while True:
+        try:
+            print("In Try")
+            # print(file_date)
+            # finra_file = finra_dir + f'{file_date}.txt'
+            # print(finra_file)
+            # ssdata_temp = get_csv(finra_file)
+            # print("FINRA File")
+            # finra_df = ssdata_temp[(ssdata_temp.TotalVolume > min_volume)]
+
+            finra_df['Percentile'] = finra_df.TotalVolume.rank(pct = True)
+            finra_df = finra_df[(finra_df.Percentile > 0.5)]
+            print(finra_df)
             src_dir = os.path.dirname(os.path.abspath(__file__))
             final_df = pd.DataFrame()
 
@@ -93,9 +109,11 @@ def get_ssdata(startdate, enddate=0):
                 mapped_df["Short%"] = mapped_df["ShortVolume"] / mapped_df["TotalVolume"]
                 mapped_df["name"] = mapped_df["Fund"] + "." + mapped_df["Symbol"]
 
+                # mapped_df.drop(
+                #     columns=['Market', "Date", "ShortVolume", "ShortExemptVolume", "Name", "Symbol", "% Holding", "Fund"],
+                #     inplace=True)
                 mapped_df.drop(
-                    columns=['Market', "Date", "ShortVolume", "ShortExemptVolume", "Name", "Symbol", "% Holding", "Fund"],
-                    inplace=True)
+                    columns=["Date", "ShortVolume", "ShortExemptVolume", "Name", "Symbol", "% Holding", "Fund"], inplace=True)
                 decimals = 2
                 mapped_df['Short%'] = mapped_df['Short%'].apply(lambda x: round(x, decimals))
 
@@ -109,8 +127,9 @@ def get_ssdata(startdate, enddate=0):
                 finra_df["name"] = "None." + finra_df["Symbol"]
                 print("HERE0")
                 finra_df.drop(
-                    columns=['Market', "Date", "ShortVolume", "ShortExemptVolume", "Symbol"],
-                    inplace=True)
+                    # columns=['Market', "Date", "ShortVolume", "ShortExemptVolume", "Symbol"],
+                    # inplace=True)
+                    columns=["Date", "ShortVolume", "ShortExemptVolume", "Symbol"], inplace=True)
                 decimals = 2
                 finra_df['Short%'] = finra_df['Short%'].apply(lambda x: round(x, decimals))
                 finra_df = finra_df[finra_df["Short%"] != 0]
@@ -131,7 +150,7 @@ def get_ssdata(startdate, enddate=0):
                 # writer.save()
 
         except requests.HTTPError as e:
-            print(f"[!] Exception caught: {e}{startdate}")
+            print(f"[!] Exception caught: {e}{file_date}")
             prior_day = datetime.strptime(file_date, '%Y%m%d') - timedelta(days=1)
             # prior_day = startdate - timedelta(days=1)
 
